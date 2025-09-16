14121f668bc2e721c2469a4dc659f4b9fea491a6
Emmanuel Lopez <emlopezdev@gmail.com>
2025-09-16 11:32:37 -0400

    updated spacing in sidebar elements and added base colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
-    return <Sidebar />;
+    return (
+        <main style={{ display: "flex" }}>
+            <Sidebar />
+            <div>Hello</div>
+        </main>
+    );
 }
 
 export default App;
